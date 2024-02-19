b4308d2858995a998516d0143d741dfa9e42ea76
ulrychz <ulrychz@KL213P11-KVD>
2024-02-19 14:32:38 +0100

    Přidejte soubory projektu.

---
+++ Po1300Obdelnik/Model/Obdelnik.cs
@@ -0,0 +1,31 @@
+﻿namespace Po1300Obdelnik.Model
+{
+    public class Obdelnik
+    {
+        public int StranaA { get; set; }
+        public int StranaB { get; set; }
+
+        public int Obvod { get; set; }
+        public int Obsah { get; set; }
+        public double Uhlopricka { get; set; }
+
+        public void SpouctiObvod()
+        {
+            Obvod = 2 * (StranaA + StranaB);
+        }
+        public void SpouctiObsah()
+        {
+            Obsah = (StranaA * StranaB);
+        }
+        public void SpouctiUhlopricku()
+        {
+            Uhlopricka = Math.Sqrt(StranaA * StranaA + Math.Pow(StranaB,2));
+        }
+        public void Vypocet()
+        {
+            SpouctiObvod();
+            SpouctiObsah();
+            SpouctiUhlopricku();
+        }
+    }
+}
