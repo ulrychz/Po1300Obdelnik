568125c5ed89b6fe2fb543ef1c74ba8adf76d17a
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-02-26 13:45:09 +0100

    Predelali tridu obdelnik

---
+++ Po1300Obdelnik/Model/Obdelnik2.cs
@@ -0,0 +1,49 @@
+﻿namespace Po1300Obdelnik.Model
+{
+    public class Obdelnik2
+    {
+        private int stranaA;
+        private int stranaB;
+
+        public int StranaA
+        {
+            get => stranaA; 
+            set
+            {
+                if (stranaA != value)
+                {
+                    if (value >= 0)
+                    {
+                        stranaA = value;
+
+                    }
+                    else
+                    {
+                        stranaA = 0;
+                    }
+                }
+            }
+        }
+        public int StranaB
+        {
+            get => stranaB; 
+            set
+            {
+                if (value >= 0)
+                {
+                    stranaB = value;
+
+                }
+                else
+                {
+                    stranaB = 0;
+                }
+            }
+        }
+
+        public int Obvod => 2 * (StranaA + StranaB);
+        public int Obsah => StranaA * StranaB;
+        public double Uhlopricka => Math.Sqrt(StranaA * StranaA + Math.Pow(StranaB, 2));
+
+    }
+}
